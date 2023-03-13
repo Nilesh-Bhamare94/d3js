56b8f5dcaa9c2b9dcaa88b1adb402b76a64ca5a0
Nilesh-Bhamare94 <nilesh.bhamare@creolestudios.com>
2023-03-13 15:14:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Chart from "./components/Bar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <Chart />
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
